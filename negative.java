d21535efdcda71fb8ee3e6d16b9aefb0c9f350fa
NishaUday <31572035+NishaUday@users.noreply.github.com>
2017-09-02 23:24:49 +0530

    Add files via upload

---
+++ negative.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class g2 {
+public static void  main(String a[])
+{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	if(n<0)
+	{
+		System.out.print("negative");
+	}
+	else if(n>=0)
+		{
+		System.out.print("positive");
+	}
+	
+}
+}
