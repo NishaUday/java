9d6a77ca79057352e34d6c57934d26a167f879d3
NishaUday <31572035+NishaUday@users.noreply.github.com>
2017-09-03 21:43:15 +0530

    Add files via upload

---
+++ sumint.java
@@ -0,0 +1,20 @@
+public class g11 {
+public static void main(String a[])
+{
+	int i,s=0,j,s1=0;
+	for(i=0;i<15;i++)
+	{
+	  s=s+i;
+	}
+	System.out.print(s);
+for(j=15;j<45;j++)
+	{
+		if(i%2!=0)
+		{
+			s1=s1+i;
+		}
+	}
+	System.out.print(s);
+}
+
+}
