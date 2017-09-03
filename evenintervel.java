d9b993d76637e1505958b8796e38dcb2e23e07c8
NishaUday <31572035+NishaUday@users.noreply.github.com>
2017-09-03 15:07:22 +0530

    Add files via upload

--- evenintervel.java
+++ evenintervel.java
@@ -6,19 +6,13 @@ Scanner sc=new Scanner(System.in);
 int i;
  int n1=sc.nextInt();
  int n2=sc.nextInt();
-/* for(i=n1;i<n2;i++)
+ for(i=n1;i<n2;i++)
 {
      if(i%2==0)
         {
         System.out.print(i); 
         }
-}*/
- for(i=n1;i<n2;i++)
- {
-	 if(i%2!=0)
-	 {
-		 System.out.print(i);
-	 }
- }
+}
+ 
 }
 }
