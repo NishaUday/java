b0a12e40bb4089a5f2a2392327e6b0848534bd65
NishaUday <31572035+NishaUday@users.noreply.github.com>
2017-09-03 22:20:20 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,16 @@
+
+public class g14 {
+	public static void main(String a[])
+	{
+		char c;
+		for(c='A';c<'Z';c++)
+		{
+			System.out.println((byte)c);
+		}
+		for(c='a';c<'z';c++)
+		{
+			System.out.println((byte)c);
+		}
+	}
+
+}
