c242adbd648218138d5f3ba2e7ebbacf303d0097
NishaUday <31572035+NishaUday@users.noreply.github.com>
2017-09-03 20:53:30 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class g8
+ {
+public static void main(String a[])
+Scanner sc=new Scanner(System.in);
+double n=sc.nextDouble();
+double n2=sc.nextDouble();
+double b=Math.pow(n,n2);
+System.out.print(b);
+	}
+	
