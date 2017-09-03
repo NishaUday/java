75db7d2e46bdc7ef3942449271b2da1739f4186f
NishaUday <31572035+NishaUday@users.noreply.github.com>
2017-09-03 21:35:52 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+public class g1 {
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int i,s=0;
+for(i=0;i<=n;i++)
+{
+    s=s+i;
+}
+System.out.print(s);
+}
+}
