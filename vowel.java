4f6e4debc5fad19648fadb7ba7e962e1c952375a
NishaUday <31572035+NishaUday@users.noreply.github.com>
2017-09-03 11:08:35 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class g7 {
+	public static void main(String a[])
+			{
+			Scanner sc=new Scanner(System.in);
+			char ch=sc.next().charAt(0);
+			if((ch=='a')||(ch=='A')||(ch=='e')||(ch=='E')||(ch=='I')||(ch=='i')||(ch=='o')||(ch=='O')||(ch=='u')||(ch=='U'))
+			{
+				System.out.print("vowel");
+	}
+			else
+			{
+				System.out.print("consonent");
+			}
+			}
+
+}
