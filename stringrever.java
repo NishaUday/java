5fafbe0be962790617e350c7b555b91dc45c72bb
NishaUday <31572035+NishaUday@users.noreply.github.com>
2017-10-05 15:05:38 +0530

    Add files via upload

---
+++ stringrever.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class g17 {
+	public static void main(String arr[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		StringBuilder s=new StringBuilder(str);
+		System.out.print(s.reverse());
+		
+	}
+
+}
