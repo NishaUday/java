1ce0956c034f7ca0a43609efbacf54e3f7c084f0
NishaUday <31572035+NishaUday@users.noreply.github.com>
2017-10-07 18:07:41 +0530

    Add files via upload

---
+++ oddnum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class g18 {
+	public static void main(String arr[])
+	{
+		int i;
+	Scanner sc=new Scanner(System.in);
+	int n1=sc.nextInt();
+	int n2=sc.nextInt();
+	for(i=n1;i<n2;i++)
+	{
+		if(i%2!=0)
+		{
+			System.out.println(i);
+		}
+	}
+	
+	}
+
+}
